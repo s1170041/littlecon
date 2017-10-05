ac4e794b0d2bdbc1675f07426c1d9dc6d15142c9
yamadakenji <s1170041@gmail.com>
2017-10-05 21:39:17 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_admin!
  	protect_from_forgery with: :exception
 end
--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,6 @@
 class PartiesController < ApplicationController
   before_action :set_party, only: [:show, :edit, :update, :destroy]
+  
 
   # GET /parties
   # GET /parties.json
