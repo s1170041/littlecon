a4463e58b4ade9809d4849e8a8b67153257a7cad
yamadakenji <s1170041@gmail.com>
2017-10-16 10:03:23 +0900

    リレーション

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   resources :enquetes
-  resources :profiles
   resources :contacts
   resources :pages
   resources :favorites
   resources :notices
   resources :parties
-  devise_for :users
+
+  devise_for :users, :controllers => {
+    :sessions => 'users/sessions',
+    :registrations => 'users/registrations'
+  }
+
   get 'parties/index'
 
   devise_for :admins
