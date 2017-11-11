bae20df394f27f8aedece24a10cb810046774802
yamadakenji <s1170041@gmail.com>
2017-11-11 20:57:43 +0900

    バリデーションを追加

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,16 @@
 class Participant < ApplicationRecord
 	belongs_to :party
+
+	# バリデーション
+	validates :entry_count,
+	presence: { message: "参加人数が入力されていません" }
+
+	validates :hoby,
+	presence: { message: "趣味が入力されていません" },
+	length:   { maximum: 100, message: "趣味は100文字以内で入力してください" }
+	
+	validates :self_pr,
+	presence: { message: "自己PRが入力されていません" },
+	length:   { maximum: 100, message: "自己PRは100文字以内で入力してください" }
+
 end
